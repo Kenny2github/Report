9261d004d671b700510c945a33bef99b54ef6c85
Kenny2github <kenny2minecraft@gmail.com>
2021-05-24 15:34:37 +0100

    Add special page name translations

---
+++ Report.i18n.alias.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Aliases for Report
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = [];
+
+/** English
+ * @author Ken Hilton
+*/
+$specialPageAliases['en'] = [
+	'Report' => [ 'Report' ],
+	'HandleReports' => [ 'HandleReports', 'Handle Reports' ]
+];
+
+/** Japanese
+ * @author apple502j
+ */
+$specialPageAliases['ja'] = [
+	'Report' => [ '報告' ],
+	'HandleReports' => [ '報告された版の確認' ]
+];
